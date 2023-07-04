b062848cbdb4e39ce7c10598ec05004221c6e834
ALEXYZ-P <alexander.geoffrey.01@gmail.com>
2023-07-04 14:45:26 +0700

    Update edit_anggota.php

--- application/views/anggota/edit_anggota.php
+++ application/views/anggota/edit_anggota.php
@@ -93,6 +93,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                   </div>
                 </div>
               </div>
+
+              <div class="form-group">
+                  <label>No Handphone</label>
+                  <input name="nohp" class="form-control <?php echo form_error('nohp') ? 'is-invalid':'' ?>" placeholder="Masukan No Handphone" value="<?php echo $anggota->nohp?>" type="text"/>
+                  <div class="invalid-feedback">
+                    <?php echo form_error('alamat')?>
+                  </div>
+                </div>
+              </div>
               <!-- /.box-body -->
 
               <div class="box-footer">
