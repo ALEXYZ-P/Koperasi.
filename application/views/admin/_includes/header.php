d36511bf4158065c6767717a9cc6db827b4c8c3a
ALEXYZ-P <alexander.geoffrey.01@gmail.com>
2023-08-01 14:01:02 +0700

    tabungan

--- application/views/admin/_includes/header.php
+++ application/views/admin/_includes/header.php
@@ -60,7 +60,7 @@
               <!-- Menu Footer-->
               <li class="user-footer">
                 <div class="pull-left">
-                  <a href="#" class="btn btn-default btn-flat">Profile</a>
+                  <!-- <a href="#" class="btn btn-default btn-flat">Profile</a> -->
                 </div>
                 <div class="pull-right">
                   <a href="<?php echo base_url('Auth/logout') ?>" class="btn btn-default btn-flat">Sign out</a>
